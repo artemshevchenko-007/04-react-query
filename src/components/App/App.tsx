7d90d543a34dfbc380bb30d3306eba57e140741e
Artem Shevchenko <tema.ua1999@gmail.com>
2026-07-01 17:24:53 +0200

    adding pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import css from "./App.module.css";
 import type { Movie } from "../../types/movie";
 import { Toaster, toast } from "react-hot-toast";
-import {
-    fetchMovies
-} from "../../services/movieService";
+
+import { fetchMovies } from "../../services/movieService";
+
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import Loader from "../Loader/Loader";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import SearchBar from "../SearchBar/SearchBar";
 import MovieModal from "../MovieModal/MovieModal";
+import Pagination from "../Pagination/Pagination";
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
@@ -17,34 +18,72 @@ export default function App() {
   const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleSelect = (movie: Movie) => setSelectedMovie(movie);
-  const handleSubmit = async (query: string) => {
+  const [query, setQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const loadMovies = async (searchQuery: string, page: number) => {
     try {
-      setMovies([]);
-      setIsError(false);
       setIsLoading(true);
-        const res = await fetchMovies(query);
-        
-         if (res.length === 0) {
-      toast.error("No movies found for your request.");
-    }
-      setMovies(res);
+      setIsError(false);
+
+      const res = await fetchMovies(searchQuery, page);
+
+      if (res.results.length === 0) {
+        toast.error("No movies found for your request.");
+        setMovies([]);
+        setTotalPages(0);
+        return;
+      }
+
+      setMovies(res.results);
+      setTotalPages(res.total_pages);
     } catch {
       setIsError(true);
     } finally {
       setIsLoading(false);
     }
   };
+
+  const handleSubmit = async (searchQuery: string) => {
+    setQuery(searchQuery);
+    setCurrentPage(1);
+
+    await loadMovies(searchQuery, 1);
+  };
+
+  const handlePageChange = async (page: number) => {
+    setCurrentPage(page);
+
+    await loadMovies(query, page);
+  };
+
+  const handleSelect = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
+
   return (
     <div className={css.app}>
-      {" "}
       <SearchBar onSubmit={handleSubmit} />
+
       <Toaster />
+
       {isError && <ErrorMessage />}
+
       {isLoading && <Loader />}
+
       {movies.length > 0 && (
-        <MovieGrid movies={movies} onSelect={handleSelect} />
+        <>
+          <MovieGrid movies={movies} onSelect={handleSelect} />
+
+          <Pagination
+            totalPages={totalPages}
+            currentPage={currentPage}
+            onPageChange={handlePageChange}
+          />
+        </>
       )}
+
       {selectedMovie && (
         <MovieModal
           movie={selectedMovie}
@@ -54,4 +93,3 @@ export default function App() {
     </div>
   );
 }
-
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -2,17 +2,27 @@ import axios from "axios";
 import type { Movie } from "../types/movie";
 
 const TMDB_TOKEN = import.meta.env.VITE_API_KEY;
+
 axios.defaults.baseURL = "https://api.themoviedb.org/3";
 axios.defaults.headers.common["Authorization"] = `Bearer ${TMDB_TOKEN}`;
 
 interface MoviesResponse {
+  page: number;
   results: Movie[];
+  total_pages: number;
+  total_results: number;
 }
 
-export const fetchMovies = async (query: string): Promise<Movie[]> => {
+export const fetchMovies = async (
+  query: string,
+  page: number
+): Promise<MoviesResponse> => {
   const response = await axios.get<MoviesResponse>("/search/movie", {
-    params: { query },
+    params: {
+      query,
+      page,
+    },
   });
 
-  return response.data.results;
+  return response.data;
 };
